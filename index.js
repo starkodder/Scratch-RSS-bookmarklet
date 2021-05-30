ee4bbf6917a23a0de826e11dd21492dd3cf76ac8
starkodder <74370377+starkodder@users.noreply.github.com>
2021-05-30 19:05:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,36 @@ border: solid 2px;
 background-color: white;
 z-index: 100;
 `;
-box.innerHTML = `<button style="left: 75%; position: fixed; transform: translateX(-100%); margin-top: 2px;">X</button><div style="position: fixed;transform: translateX(-50%);left: 50%;/*! top: 50%; */margin-top: 3em;"><label>Add project: </label><input><button>Add</button></div><center><h2>Scratch RSS</h2></center>`
+box.innerHTML = `<button style="left: 75%; position: fixed; transform: translateX(-100%); margin-top: 2px;">X</button><div style="position: fixed;transform: translateX(-50%);left: 50%;/*! top: 50%; */margin-top: 3em;"><input placeholder="Add project (id)"><button>+</button></div>
+
+<center style=""><h2>Scratch RSS</h2></center><table style="margin-top: 3em;border: 1px solid black;position: fixed;left: 50%;transform: translateX(-50%);">
+		<style>table img{height: 1em;}</style>
+    <tbody style=""><tr>
+      <th>Title</th>
+      <th>Last updated</th>
+      <th></th>
+      
+    </tr>
+      <tr>
+      <td><a href="/labbet/2693158">SKOLARBETE</a></td>
+      <td class="updated">2021-05-27 10:43:24</td>
+      <td>
+	<img onclick="deleteFile(&quot;2693158&quot;)" class="trashcan" src="http://selma.friskola.nu/images/trashcan.png">
+      </td>
+    </tr>
+      <tr>
+      <td><a href="/labbet/89885965">sierpinski</a></td>
+      <td class="updated">2021-05-19 08:47:51</td> 
+      <td>
+	<img onclick="deleteFile(&quot;89885965&quot;)" class="trashcan" src="http://selma.friskola.nu/images/trashcan.png">
+      </td>
+    </tr>
+      <tr>
+      <td><a href="/labbet/36885973">gamepad test</a></td>
+      <td class="updated">2021-05-15 14:59:05</td>
+      <td>
+	<img onclick="deleteFile(&quot;36885973&quot;)" class="trashcan" src="http://selma.friskola.nu/images/trashcan.png">
+      </td>
+    </tr>
+</tbody></table>`
 document.body.appendChild(box);
