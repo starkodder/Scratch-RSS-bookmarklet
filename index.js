a0819050dcbea4a4b006b20a347046193557cb0c
starkodder <74370377+starkodder@users.noreply.github.com>
2021-05-30 17:09:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-element = document.createElement('div');
-element.style = `
+box = document.createElement('div');
+box.style = `
 height: 50%;
 width: 50%;
 top: 25%;
@@ -8,4 +8,17 @@ position: fixed;
 border: solid 2px;
 background-color: white;
 `;
-document.body.appendChild(element);
+document.body.appendChild(box);
+
+element = document.createElement('button');
+element.textContent = 'X';
+element.style = `
+left: 75%;
+position: fixed;
+transform: translateX(-100%);
+margin-top: 2px;
+`;
+box.appendChild(element);
+
+element = document.createElement('input');
+box.appendChild(element);
