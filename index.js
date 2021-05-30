636f7bd0c14b750d6523f03c49fd99bef7af945a
starkodder <74370377+starkodder@users.noreply.github.com>
2021-05-30 18:10:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ border: solid 2px;
 background-color: white;
 z-index: 100;
 `;
-box.innerHTML = `<button style="left: 75%; position: fixed; transform: translateX(-100%); margin-top: 2px;">X</button><label>Add project: </label><input><center>Scratch RSS</center>`
+box.innerHTML = `<button style="left: 75%; position: fixed; transform: translateX(-100%); margin-top: 2px;">X</button><div style="position: fixed;transform: translateX(-50%);left: 50%;/*! top: 50%; */margin-top: 3em;"><label>Add project: </label><input><button>Add</button></div><center><h2>Scratch RSS</h2></center>`
 document.body.appendChild(box);
